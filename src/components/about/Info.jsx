578ef66301d8f2b434ac817047bcfe96ebc2662d
hariti asmaa <72623740+hariti-asm@users.noreply.github.com>
2024-04-20 22:05:35 +0100

    Update Info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -12,7 +12,7 @@ function Info() {
       <div className="about__box">
         <i class="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">+16 Projects</span>
+        <span className="about__subtitle">+50 Projects</span>
       </div>
 
       <div className="about__box">
